779a759a730c0a4d95ebf1f56cfccf1e45f4f544
BhushanSah <itsbhushansah@gmail.com>
2026-07-01 19:23:18 -0400

    Completed the logic for pull

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -1,5 +1,34 @@
+const fs= require("fs").promises;
+const path=require("path");
+const { s3, S3_BUCKET } = require("../config/aws-config");
+
 async function pullRepo(){
-    console.log("pull command called");
+    const repoPath=path.resolve(process.cwd(), ".CodeHarbor");
+    const commitsPath=path.join(repoPath, "commits");
+
+    try{
+        const data= await s3.listObjectsV2({Bucket: S3_BUCKET, Prefix: "commits/",}).promise();
+        const objects=data.Contents || [];
+        if (objects.length === 0) {
+            console.log("No commits found in S3.");
+            return;
+        }
+
+        for( const object of objects){
+            const key=object.Key;
+            const commitDir=path.join(commitsPath, path.dirname(key).split("/").pop());
+            await fs.mkdir(commitDir, {recursive:true});
+            const params={
+                    Bucket: S3_BUCKET,
+                    Key: key,
+                };
+            const fileContent= await s3.getObject(params).promise();
+            await fs.writeFile(path.join(commitDir, path.basename(key)), fileContent.Body);
+        }
+        console.log("All commits pulled from S3");
+    }catch(err){
+        console.error("Error while pulling:", err);
+    }
 }
 
 module.exports={pullRepo};

006e16aac14f3764f878e349ac56dd634680774e
BhushanSah <itsbhushansah@gmail.com>
2026-07-03 17:49:03 -0400

    completed the logic for updateIssueByID

--- backend/controllers/issueController.js
+++ backend/controllers/issueController.js
@@ -5,6 +5,7 @@ const Issue=require("../models/issueModel");
 
 const createIssue= async(req,res)=>{
     const{ title,description}=req.body;
+    const repoID=req.params.repositoryId
     try{
         if(!title){
             return res.status(400).json({error:"Issue Title is required!"})
@@ -12,7 +13,7 @@ const createIssue= async(req,res)=>{
         if(!description){
             return res.status(400).json({error:"Issue description is required!"})
         }
-        if (!mongoose.Types.ObjectId.isValid(repositoryId)) {
+        if (!mongoose.Types.ObjectId.isValid(repoID)) {
             return res.status(400).json({
             error: "Invalid repository ID!",
             });
@@ -21,7 +22,7 @@ const createIssue= async(req,res)=>{
         const issue=new Issue({
             title,
             description, 
-            repository: req.params.repositoryId,  
+            repository: repoID,  
         });
         
         const result = await issue.save();
@@ -31,13 +32,54 @@ const createIssue= async(req,res)=>{
         })
 
     }catch(err){
-        console.error("Error during Repository creation:", err.message);
+        console.error("Error during Issue creation:", err.message);
         return res.status(500).send("Server Error");
     }
 };
 
 const updateIssueById= async(req,res)=>{
-    res.send("Update issues");
+    const id=req.params.id;
+    const{title, description, status}=req.body;
+    try{
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+            error: "Invalid issue ID!",
+            });
+        }    
+        
+       const issue=await Issue.findById(id);
+       if(!issue){
+        return res.status(400).json({error:"Issue not found!"});
+       }
+       if (title) {
+        issue.title = title;
+       }
+
+       if (description) {
+        issue.description = description;
+       }
+
+       if (status) {
+            if (status !== "open" && status !== "closed") {
+                return res.status(400).json({
+                error: "Status must be open or closed!",
+                 });
+            }
+            issue.status = status;
+        }
+
+
+       const updatedIssue=await issue.save();
+        res.status(201).json({
+            message:"Issue updated!",
+            issue: updatedIssue,
+
+        })
+
+    }catch(err){
+        console.error("Error during Issue updation:", err.message);
+        return res.status(500).send("Server Error");
+    }
 };
 
 const deleteIssueById= async(req,res)=>{
